26250ece87ea09fd7b8c2bda028fce2fac12dba5
ErshovVladislav10M <yersh2000@gmail.com>
2022-03-22 01:29:48 +0300

    Update LV_protocol.cpp

--- src/LV_protocol.cpp
+++ src/LV_protocol.cpp
@@ -16,17 +16,17 @@ float state_lv = 9;  // Используется для принятия и от
 // Переменные и счетчики
 int n_neighbors_choise = 5;  // Количество соседей, которые выбираем случайно из n_neighbors
 float alpha = 0.7;  // Коэффициент расчета протокола LV (коэффициент доверия)
-float grad = 0.2;  // Переменная для проверки синхронизации
-// Если разница между abs(ssid_name_g[i] - state) < grad для всех i, достигается синхронизация
+float epsilon = 0.2;  // Переменная для проверки синхронизации
+// Если разница между abs(ssid_name_g[i] - state) < epsilon для всех i, достигается синхронизация
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  // Если в Serial есть сигнал, то мы считываем его и записывамаем в state,
-  // считаем плату отправителем.
-  // Иначе проверяем, является ли плата отправителем.
+  // Если в Serial есть сигнал, то мы считываем его, записывамаем в state
+  // и считаем плату отправителем.
+  // Иначе проверяем, является ли плата отправителем
   if (Serial.available() > 0) {
     state = Serial.parseInt();
     state_lv = state;
@@ -45,19 +45,16 @@ void loop() {
   const char * ap_ssid = new char[6]
   {'1', '1', '1', first_number, second_number, '\0'};
 
-  // Создаем сеть с новым названием и без пароля.
-  WiFi.softAP(ap_ssid, NULL);  // Start AP mode
+  WiFi.softAP(ap_ssid, NULL);
 
-  // Сканируем сети
   int networksFound = WiFi.scanNetworks();
   // Смотрим список сетей и заполняем массивы с названием и уровнем сигнала.
   int ssid_name_g[n_neighbors_choise];
   int rssi_g[n_neighbors_choise];
   int i_g = 0;  // Количество считанных соседей, с подходящим названием
-  int rssi;  // Мощность сигнала
-  int ssid_name;  // Название сети с данными
+  int rssi;
+  int ssid_name;
 
-  // Если сети есть, считываем данные.
   if (networksFound > 0) {
     for (int i_scan = 0; i_scan < networksFound; i_scan++) {
       if (i_g >= n_neighbors_choise) {
@@ -74,23 +71,22 @@ void loop() {
     }
   }
 
-  // Обновляем данные согласно протоколу LV.
+  // Обновляем данные согласно протоколу LV
   if (i_g != 0) {
     for (int i = 0; i < i_g; i++) {
       state_lv += alpha * (ssid_name_g[i] - state_lv);
     }
   }
 
-  // Проверяем стабилизацию.
   bool is_stabilization = true;
   for (int i = 0; i < i_g; i++) {
-    if (abs(ssid_name_g[i] - state) > grad) {
+    if (abs(ssid_name_g[i] - state) > epsilon) {
       is_stabilization = false;
       break;
     }
   }
 
-  // Отправляем данные о синхронизации.
+  // Отправляем данные о синхронизации
   Serial.println("Iteration");
   if (i_g == 0) {
     Serial.println("Not found networks");
